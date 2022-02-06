504284725f35bfb13689304a2dd3adca7eb6ec77
Dénes Evelin <denes.evelin@gmail.com>
2022-02-06 16:28:33 +0100

    Regisztrációs felület kinézetének létrehozása

--- app/src/main/java/com/example/android/RegisztraciosFelulet.java
+++ app/src/main/java/com/example/android/RegisztraciosFelulet.java
@@ -3,12 +3,27 @@ package com.example.android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class RegisztraciosFelulet extends AppCompatActivity {
+
+    private Spinner SpinnerTelepules;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regisztracios_felulet);
+
+        init();
+
+        //Spinner tesztelése
+        String[] items = new String[]{"1", "2", "three"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        SpinnerTelepules.setAdapter(adapter);
+    }
+
+    public void init(){
+        SpinnerTelepules = findViewById(R.id.SpinnerTelepules);
     }
 }
