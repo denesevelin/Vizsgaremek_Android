ab9d93fdf523f1c0c97d4cd4a09af5357cfa165a
Dénes Evelin <denes.evelin@gmail.com>
2022-02-07 15:09:14 +0100

    Hirdetékes keresése felület Szűrés gomb -> Hirdetések listázása Activity létrehozása

--- app/src/main/java/com/example/android/HirdetesekKeresese.java
+++ app/src/main/java/com/example/android/HirdetesekKeresese.java
@@ -2,13 +2,17 @@ package com.example.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class HirdetesekKeresese extends AppCompatActivity {
 
     private Spinner SpinnerKategoriaKeres, SpinnerTelepulesKeres;
+    private Button buttonSzures;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +22,20 @@ public class HirdetesekKeresese extends AppCompatActivity {
         init();
         kategoriakBetoltese();
         telepulesekBetoltese();
+
+        buttonSzures.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent_szures = new Intent(HirdetesekKeresese.this, HirdetesekListazasa.class);
+                startActivity(intent_szures);
+            }
+        });
     }
 
     public void init(){
         SpinnerKategoriaKeres = findViewById(R.id.SpinnerKategoriaKeres);
         SpinnerTelepulesKeres = findViewById(R.id.SpinnerTelepulesKeres);
+        buttonSzures = findViewById(R.id.buttonSzures);
     }
 
     public void kategoriakBetoltese(){
