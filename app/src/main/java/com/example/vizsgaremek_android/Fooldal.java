d1501e606939da3c710cab65b8f4b85c58d49ab6
Dénes Evelin <denes.evelin@gmail.com>
2022-08-19 20:52:41 +0200

    Főoldal bejelentkezés és Regisztráció gombjának beüzemelése

--- app/src/main/java/com/example/vizsgaremek_android/Fooldal.java
+++ app/src/main/java/com/example/vizsgaremek_android/Fooldal.java
@@ -4,17 +4,49 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class Fooldal extends Fragment {
+
+    private Button buttonBejelentkezésFooldal, buttonRegisztracioFooldal;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fooldal, container, false);
+
+        buttonBejelentkezésFooldal = view.findViewById(R.id.buttonBejelentkezésFooldal);
+        buttonRegisztracioFooldal = view.findViewById(R.id.buttonRegisztracioFooldal);
+
+        //Bejelentkezési felület megnyitása
+        buttonBejelentkezésFooldal.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                FragmentTransaction fr = getFragmentManager().beginTransaction();
+                fr.replace(R.id.fragment_container, new Bejelentkezes());
+                fr.commit();
+            }
+        });
+
+        //Regisztrációs felület megnyitása
+        buttonRegisztracioFooldal.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                FragmentTransaction fr = getFragmentManager().beginTransaction();
+                fr.replace(R.id.fragment_container, new Regisztracio());
+                fr.commit();
+            }
+        });
+
         return view;
     }
 }
