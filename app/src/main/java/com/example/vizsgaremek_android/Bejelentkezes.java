0b444ce066133a9bf34f35f188db070ed01ca012
Dénes Evelin <denes.evelin@gmail.com>
2022-08-26 16:17:29 +0200

    Bejelentkezési oldalon a gomb aktiválása

--- app/src/main/java/com/example/vizsgaremek_android/Bejelentkezes.java
+++ app/src/main/java/com/example/vizsgaremek_android/Bejelentkezes.java
@@ -4,17 +4,36 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class Bejelentkezes extends Fragment {
+
+    private Button buttonBejelentkezesBejelentkezes;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.bejelentkezes, container, false);
+
+        buttonBejelentkezesBejelentkezes = view.findViewById(R.id.buttonBejelentkezesBejelentkezes);
+
+        //Hirdetések keresése oldal megnyitása
+        buttonBejelentkezesBejelentkezes.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                FragmentTransaction fr = getFragmentManager().beginTransaction();
+                fr.replace(R.id.fragment_container, new HirdetesekKeresese());
+                fr.commit();
+            }
+        });
+
         return view;
     }
 }
