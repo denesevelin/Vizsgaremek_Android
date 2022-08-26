54ff47ac1eceec5faaa6bfa71f13ddf04f1857af
Dénes Evelin <denes.evelin@gmail.com>
2022-08-26 16:45:18 +0200

    Hirdetések listázása oldalon a gomb aktiválása; Konkrét hirdetés java file létrehozása

--- app/src/main/java/com/example/vizsgaremek_android/HirdetesekListazasa.java
+++ app/src/main/java/com/example/vizsgaremek_android/HirdetesekListazasa.java
@@ -4,17 +4,34 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class HirdetesekListazasa extends Fragment {
+
+    private Button buttonTovabbHirdetesekListazasa;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.hirdetesek_listazasa, container, false);
+
+        buttonTovabbHirdetesekListazasa = view.findViewById(R.id.buttonTovabbHirdetesekListazasa);
+
+        //Konkrét hirdetésre
+        buttonTovabbHirdetesekListazasa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fr = getFragmentManager().beginTransaction();
+                fr.replace(R.id.fragment_container, new KonkretHirdetes());
+                fr.commit();
+            }
+        });
+
         return view;
     }
 }
