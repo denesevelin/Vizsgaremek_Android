2cd24f6d36bb472d85d28a082bad4094ace912e8
Dénes Evelin <denes.evelin@gmail.com>
2022-09-03 23:51:45 +0200

    Kategoria osztály létrehozása

---
+++ app/src/main/java/com/example/vizsgaremek_android/Kategoria.java
@@ -0,0 +1,37 @@
+package com.example.vizsgaremek_android;
+
+public class Kategoria {
+    private int kategoria_id;
+    private String kategoria_nev;
+    private String kategoria_kep;
+
+    public Kategoria(int kategoria_id, String kategoria_nev, String kategoria_kep) {
+        this.kategoria_id = kategoria_id;
+        this.kategoria_nev = kategoria_nev;
+        this.kategoria_kep = kategoria_kep;
+    }
+
+    public int getKategoria_id() {
+        return kategoria_id;
+    }
+
+    public void setKategoria_id(int kategoria_id) {
+        this.kategoria_id = kategoria_id;
+    }
+
+    public String getKategoria_nev() {
+        return kategoria_nev;
+    }
+
+    public void setKategoria_nev(String kategoria_nev) {
+        this.kategoria_nev = kategoria_nev;
+    }
+
+    public String getKategoria_kep() {
+        return kategoria_kep;
+    }
+
+    public void setKategoria_kep(String kategoria_kep) {
+        this.kategoria_kep = kategoria_kep;
+    }
+}
