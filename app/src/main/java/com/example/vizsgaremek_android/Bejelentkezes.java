893ab777997a486e64a55c9c9b88e1f72c2be579
Dénes Evelin <denes.evelin@gmail.com>
2022-09-03 19:58:08 +0200

    Bejelentkezés oldalon az Adatletöltő osztály bejelentkezés metódusának meghívása + Toast üzenetek létrehozása

--- app/src/main/java/com/example/vizsgaremek_android/Bejelentkezes.java
+++ app/src/main/java/com/example/vizsgaremek_android/Bejelentkezes.java
@@ -5,6 +5,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,6 +17,7 @@ import androidx.fragment.app.FragmentTransaction;
 public class Bejelentkezes extends Fragment {
 
     private Button buttonBejelentkezesBejelentkezes;
+    private EditText editFelhasznalonevBejelentkezes, editJelszoBejelentkezes;
 
     @Nullable
     @Override
@@ -21,17 +25,24 @@ public class Bejelentkezes extends Fragment {
         View view = inflater.inflate(R.layout.bejelentkezes, container, false);
 
         buttonBejelentkezesBejelentkezes = view.findViewById(R.id.buttonBejelentkezesBejelentkezes);
+        editFelhasznalonevBejelentkezes = view.findViewById(R.id.editFelhasznalonevBejelentkezes);
+        editJelszoBejelentkezes = view.findViewById(R.id.editJelszoBejelentkezes);
 
-        //Hirdetések keresése oldal megnyitása
-        buttonBejelentkezesBejelentkezes.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                FragmentTransaction fr = getFragmentManager().beginTransaction();
-                fr.replace(R.id.fragment_container, new HirdetesekKeresese());
-                fr.commit();
-            }
+        buttonBejelentkezesBejelentkezes.setOnClickListener(view1 -> {
+            String felhnev = editFelhasznalonevBejelentkezes.getText().toString();
+            String jelszo = editJelszoBejelentkezes.getText().toString();
+
+            new Adatletolto().bejelentkezes(getContext(),felhnev, jelszo, Successful -> {
+                if (Successful){
+                    Toast.makeText(getActivity(), "Sikeres bejelentkezés!", Toast.LENGTH_SHORT).show();
+                    FragmentTransaction fr = getFragmentManager().beginTransaction();
+                    fr.replace(R.id.fragment_container, new HirdetesekKeresese());
+                    fr.commit();
+                }
+                else {
+                    Toast.makeText(getActivity(), "Sikertelen bejelentkezés!", Toast.LENGTH_SHORT).show();
+                }
+            });
         });
 
         return view;
