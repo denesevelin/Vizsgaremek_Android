6a0e639abed75d3e6e449fbd3a24743c86a99461
Dénes Evelin <denes.evelin@gmail.com>
2022-09-07 02:27:37 +0200

    Profilhoz tartozó fájlokból a felesleges kódok törlése + felhasználó törlése után kiléptetés

--- app/src/main/java/com/example/vizsgaremek_android/Ranglista.java
+++ app/src/main/java/com/example/vizsgaremek_android/Ranglista.java
@@ -1,10 +1,8 @@
 package com.example.vizsgaremek_android;
 
-import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
